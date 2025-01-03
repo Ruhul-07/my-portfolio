3fe02d3e91fc5b561cfda8a41b4c8a2554e60c37
Ruhul Amin <ruhulaminbabu93@gmail.com>
2025-01-04 03:15:16 +0600

    Initial project setup and some package installed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./assets/Ruhul_Amin_logo.png"
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <img src={logo} alt="" className="w-40"/>
+    </>
+  )
+}
+
+export default App
